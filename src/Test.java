5c33ed907928dc6074c916d6f16912cd9011d1a6
BushraOnika <bushrahossainonika@gmail.com>
2021-07-14 21:29:59 +0600

    init commit

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Account ac = new Account(1234, 2342);
+        Account ac2 = new Account(213546);
+        System.out.println(ac2);
+        ac2.credit(2000);
+        System.out.println(ac2);
+    }
+}
